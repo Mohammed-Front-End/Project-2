5c70402f85c190434880401e408c049ba4fb3477
mohabbro <elabedymidoxl@gmail.com>
2024-06-05 21:39:17 +0300

    add file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base:"/e-ecommerce-by-React-and-typescript/",
+  base:"/e-ecommerce-by-React-and-typescript/",
   plugins: [react()],
+
 })
